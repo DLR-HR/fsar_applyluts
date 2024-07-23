23ae0df3c845a80c17bb0faea667fe9dacbee586
ultimarc <176302663+ultimarc@users.noreply.github.com>
2024-07-23 14:57:45 +0200

    - simplified LUT selection - skip blocks for which no input data is available - allow empty band CLI option (for single-band data sets) - shortened CLI option names - provide default values for CLI options where applicable

--- fsar_applyLUTs/geocoding.py
+++ fsar_applyLUTs/geocoding.py
@@ -26,7 +26,7 @@ import re
 import numpy as np
 import rasterio
 import rio_cogeo
-import os
+import os, glob
 import scipy
 from rasterio.windows import Window
 
@@ -40,73 +40,20 @@ def get_lookup_tables(gtc_lut_path, band):
     filename_band i.e. sr2geo_az_P
     """
 
-    filename_patterns = ["sr2geo_az", "sr2geo_rg", "geo2sr_east", "geo2sr_north"]
-    # every band the input could be in
-    band_patterns = r"_(X|C|S|L|P)"
+    lut_names = ["sr2geo_az", "sr2geo_rg", "geo2sr_east", "geo2sr_north"]
+    band_re = f"_{band.upper()}_" if len(band) > 0 else r"_(X|C|S|L|P)_"
 
-    filename_pattern = '|'.join(filename_patterns)
     lookup_tables = {}
-
-    for filename in os.listdir(gtc_lut_path):
-        if band is not None and f"_{band}_" not in filename:
+    for lut in lut_names:
+        lut_files = glob.glob(os.path.join(gtc_lut_path, lut)+'*')
+        lut_files = [f for f in lut_files if re.search(band_re, f) is not None]
+        if len(lut_files) > 1:
+            raise ValueError(f'Multiple "{lut}" LUTs found. Specify frequency band with --band!')
+        elif len(lut_files) == 0:
             continue
-
-        filename_match = re.search(filename_pattern, filename)
-        if filename_match:
-            band_match = re.search(band_patterns, filename)
-            if band_match:
-                if filename.endswith(".rat") or filename.endswith(".tif"):
-                    found_file = filename_match.group() + band_match.group()
-                    lookup_tables[found_file] = os.path.join(gtc_lut_path, filename)
+        lookup_tables[lut] = lut_files[0]
 
     return lookup_tables
-
-
-def get_band_luts(found_table, lookup_tables):
-    """
-    function that gets all lookup tables that have the same frequency band as the provided lookup table.
-    :param found_table: lookup table with the frequency band that's being searched for
-    :param lookup_tables: dictionary containing all lookup tables that could be found
-    :return: dictionary containing all four lookup tables for this frequency band
-    """
-    last_underscore_index = found_table.rfind('_')
-
-    band = found_table[last_underscore_index:]
-    all_lookup_tables = {}
-
-    for lookup_table in lookup_tables:
-        match = re.search(band, lookup_table)
-        if match:
-            # has to be done again here because the lookup_table string doesn't have to share the length of the found_table
-            last_underscore_index = lookup_table.rfind('_')
-            lookup_table_name = lookup_table[:last_underscore_index]
-            all_lookup_tables[lookup_table_name] = lookup_tables[lookup_table]
-
-    if len(all_lookup_tables) < 2:
-        raise NotImplementedError("couldn't find all necessary lookup tables")
-
-    return all_lookup_tables
-
-
-def get_lut_for_input(lookup_tables, input_file):
-    """
-    function that takes in all lookup tables for all frequency bands and only returns the ones for the matching frequency
-    band.
-    :param lookup_tables: dictionary containing the paths to all lookup tables that could be found
-    :param input_file: path to the input file
-    :return: the four lookup tables of the frequency band, the input file has matched
-    """
-
-    with rasterio.open(input_file) as input_file:
-        input_shape = input_file.shape
-
-    for lookup_table in lookup_tables:
-        with rasterio.open(lookup_tables[lookup_table]) as lookup_file:
-            lookup_shape = lookup_file.shape
-
-        # if input and lookup have the same shape that means they would have the same geometry
-        if input_shape != lookup_shape:
-            return get_band_luts(lookup_table, lookup_tables)
 
 
 def calculate_window_limits(window, max_size):
@@ -155,16 +102,16 @@ def write_to_file(offset_ax1, offset_ax2, blocksize, order, first_axis, second_a
 
             min_ax1, width = calculate_window_limits(ax1_window, input_file.shape[0])
             min_ax2, height = calculate_window_limits(ax2_window, input_file.shape[1])
-
-            # read data chunk starting from the smallest value necessary, to the biggest value necessary
-            for i in range(1, input_file.profile["count"] + 1):
-                input_window = input_file.read(i, window=Window(min_ax2, min_ax1, height, width))
-                # axis - min to get from coordinates that would apply for the whole input matrix to coordinates
-                # which apply for this part of the input matrix
-                output = scipy.ndimage.map_coordinates(input_window,
-                                                       [ax1_window - min_ax1, ax2_window - min_ax2],
-                                                       order=order)
-                dst.write(output, i, window=window)
+            if width > 0 and height > 0:
+                # read data chunk starting from the smallest value necessary, to the biggest value necessary
+                for i in range(1, input_file.profile["count"] + 1):
+                    input_window = input_file.read(i, window=Window(min_ax2, min_ax1, height, width))
+                    # axis - min to get from coordinates that would apply for the whole input matrix to coordinates
+                    # which apply for this part of the input matrix
+                    output = scipy.ndimage.map_coordinates(input_window,
+                                                           [ax1_window - min_ax1, ax2_window - min_ax2],
+                                                           order=order)
+                    dst.write(output, i, window=window)
 
 
 def process_blockwise(input_file, output_file, lookup_tables, to_slant_range, blocksize, order, default_order):
@@ -232,37 +179,31 @@ def main():
     default_blocksize = 512
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--gtc_lut",
+    parser.add_argument("--dir", choices=['sr2geo', 'geo2sr'],
+                        help="sr2geo: map from slantrange to UTM grid (default); "
+                             "geo2sr: map from UTM grid to slantrange", default='sr2geo')
+    parser.add_argument("--luts",
                         help=f"path to the GTC-LUT directory", required=True)
-    parser.add_argument("--input_file",
+    parser.add_argument("--band", choices=['X','C','S','L','P',''], default='',
+                        help=f"The frequency band of the input file (defaults to '', "
+                             f"which works when the geocoded product only contains one band)")
+    parser.add_argument("--in", dest="input_file",
                         help=f"absolute path to the input file", required=True)
-    parser.add_argument("--output_file",
+    parser.add_argument("--out", dest="output_file",
                         help=f"absolute path to the output file", required=True)
-    parser.add_argument("--band", help=f"The frequency band of the input file (for example P, X, S or L)",
-                        required=True)
     parser.add_argument("--order", help=f"order of the spline interpolation, has to be between 0-5. The default is "
                                         f"{default_order} for floating point data and 0 for integer data",
                         required=False, type=int)
     parser.add_argument("--blocksize", help=f"the size per processed chunk of data. Defaults to {default_blocksize}",
                         default=default_blocksize, type=int, required=False)
-    parser.add_argument("--geometry", help=f"type geo if you want to go from geocoded to slant range "
-                                           f"or sr if you want to go from slant range to geocoded.",
-                        required=True)
     args = parser.parse_args()
 
-    lookup_tables = get_lookup_tables(args.gtc_lut, args.band)
+    lookup_tables = get_lookup_tables(args.luts, args.band)
 
-    if args.geometry == "geo":
-        to_slant_range = True
-        lookup_tables = get_lut_for_input(lookup_tables, args.input_file)
-    elif args.geometry == "sr":
-        to_slant_range = False
-        lookup_tables = get_lut_for_input(lookup_tables, args.input_file)
-    else:
-        raise ValueError(f"You typed {args.geometry} in as the geometry but only sr or geo are viable options.")
-
-    process_blockwise(args.input_file, args.output_file, lookup_tables, to_slant_range, args.blocksize, args.order,
-                      default_order)
+    process_blockwise(
+        args.input_file, args.output_file, lookup_tables, args.dir == "geo2sr",
+        args.blocksize, args.order, default_order
+    )
 
 
 if __name__ == "__main__":
